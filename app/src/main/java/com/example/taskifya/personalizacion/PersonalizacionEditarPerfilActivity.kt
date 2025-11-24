01e04bc3935383b4271d23930649895db0f0379d
TITA FA <217063174+TITAFA@users.noreply.github.com>
2025-11-24 11:04:59 -0500

    actualizacion

--- app/src/main/java/com/example/taskifya/personalizacion/PersonalizacionEditarPerfilActivity.kt
+++ app/src/main/java/com/example/taskifya/personalizacion/PersonalizacionEditarPerfilActivity.kt
@@ -5,17 +5,23 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taskifya.R
 
+
 class PersonalizacionEditarPerfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personalizacion_editar_perfil)
 
+// Recibir correo del usuario (enviado desde Login o MainActivity)
+        val correoUsuario = intent.getStringExtra("correo")
 
-        // Botón que abre Editar Perfil
-        findViewById<Button>(R.id.buttonEditarPerfil).setOnClickListener {
-            val intent = Intent(this, EditarPerfilActivity::class.java)
-            startActivity(intent)
+        val btnEditarPerfil = findViewById<Button>(R.id.buttonEditarPerfil)
 
+        btnEditarPerfil.setOnClickListener {
+
+            // Enviar el correo a TU pantalla de edición
+//            val intent = Intent(this, EditarPerfilActivity::class.java)
+//            intent.putExtra("correo", correoUsuario)
+//            startActivity(intent)
+          }
         }
-    }
 }
