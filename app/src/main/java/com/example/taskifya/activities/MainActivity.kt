01a285e542c88356b13216e1053b4ea551f7279d
Valentin <yumejv@gmail.com>
2026-03-22 10:04:00 -0500

    renombrado y correcion

--- app/src/main/java/com/example/taskifya/activities/MainActivity.kt
+++ app/src/main/java/com/example/taskifya/activities/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_notas)
 
         // Inicializar vistas
         recyclerViewDias = findViewById(R.id.recyclerViewDias)
